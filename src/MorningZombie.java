067a0c53c27ed5080cdbf94757136058c10fd4e8
angelvillarreal1 <league@Mac-mini-4.local>
2017-05-13 11:25:45 -0700

    lol

---
+++ src/MorningZombie.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+	public static void main(String[] args) {
+		System.out.println("Sup");
+		JOptionPane.showMessageDialog(null,"First, brush your teeth and change your clothes");
+		JOptionPane.showMessageDialog(null,"Then, you eat breakfest and you pack up your bag");
+		JOptionPane.showMessageDialog(null,"Next, you have to walk to school without chasing anything");
+		JOptionPane.showMessageDialog(null,"Then,");
+		JOptionPane.showMessageDialog(null,"");
+		JOptionPane.showMessageDialog(null,"");
+		
+				
+				
+				
+				
+	    
+	}
+
+}
---
+++ src/knockknockjokes.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+public class knockknockjokes {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Knock knock");
+		JOptionPane.showInputDialog("dishes");
+		JOptionPane.showInputDialog("dishes a very bad joke");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
