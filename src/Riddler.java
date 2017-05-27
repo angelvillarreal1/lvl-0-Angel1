b18957a40e5f8ae32d7a06e2886c1c618adf6418
Kendra Kleber <league@Mac-mini-2.local>
2017-05-27 11:26:49 -0700

    27 may 2017
    riddle

---
+++ src/Riddler.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Riddler {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("if h20 is formula for water what is the formula for ice?");
+		JOptionPane.showInputDialog("h20 cubed");
+		JOptionPane.showInputDialog("why did the math book suicide");
+		JOptionPane.showInputDialog("because it had too many problems");
+		JOptionPane.showInputDialog("where do fish go on vacation");
+		JOptionPane.showInputDialog("finland");
+		JOptionPane.showInputDialog("where do songbirds go on vacation");
+		JOptionPane.showInputDialog("the canary islands");
+		JOptionPane.showInputDialog("where do zombies go on vaction");
+		JOptionPane.showInputDialog("the dead islands");
+		JOptionPane.showInputDialog("where do thanksgiving birds go on vacation");
+		JOptionPane.showInputDialog("turkey");
+		JOptionPane.showInputDialog("where do geometry teachers go on vacation");
+		JOptionPane.showInputDialog("cuba");
+		JOptionPane.showInputDialog("where do locksmiths go on vacation");
+		JOptionPane.showInputDialog("the florida keys");
+		JOptionPane.showInputDialog("");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Warmup.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"How to solve a Rubiks cube");
+		JOptionPane.showMessageDialog(null,"you solve the green");
+		JOptionPane.showMessageDialog(null,"Then you move up to red and yellow");
+		JOptionPane.showMessageDialog(null,"Then white and orange");
+		JOptionPane.showMessageDialog(null,"finally you solve the blue");
+		JOptionPane.showMessageDialog(null,"I like turtles");
+		JOptionPane.showMessageDialog(null,"How to die");
+		JOptionPane.showMessageDialog(null,"first you die");		
+		JOptionPane.showMessageDialog(null,"Thank you for reading all of my messages");		
+		JOptionPane.showMessageDialog(null,"now you will die :) o==()==>");		
+		JOptionPane.showMessageDialog(null,"yes, that is a sword");		
+		JOptionPane.showMessageDialog(null,"i know i am bad at making swords");		
+		JOptionPane.showMessageDialog(null,"i mean i just had to try");		
+		JOptionPane.showMessageDialog(null,"i quit my job im out of here!");		
+		JOptionPane.showtInputdialog
+		
+		
+		
+	}
+
+}
