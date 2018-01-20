6a545fb0cdccb673673363ffcc08201de8009ed5
DemoliciousDt <league@Thes-Mac-mini.local>
2018-01-20 11:25:56 -0800

    learned code 14
    finished OR

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -6,57 +6,167 @@ public class ObedientRobot {
 	public static void main(String[] args) {
 		String s = JOptionPane.showInputDialog("What shape do you want? square triangle or circle");
 		if (s.equals("square")) {
-			Robot a = new Robot();
-			a.setSpeed(10);
-			a.move(50);
-			a.penDown();
-			a.turn(90);
-			a.move(50);
-			a.turn(90);
-			a.move(100);
-			a.turn(90);
-			a.move(100);
-			a.turn(90);
-			a.move(100);
-			a.turn(90);
-			a.move(50);
-			a.penUp();
-			a.turn(-90);
-			a.move(1000);
-		}
-		else if (s.equals("triangle")) {
-			Robot a = new Robot();
-			a.setSpeed(10);
-			a.turn(180);
-			a.move(50);
-			a.penDown();
-			a.turn(90);
-			a.move(50);
-			a.turn(120);
-			a.move(100);
-			a.turn(120);
-			a.move(100);
-			a.turn(120);
-			a.move(50);
-			a.penUp();
-			a.turn(90);
-			a.move(1000);
-		}
-		else if(s.equals("circle")) {
-			Robot a = new Robot();
-			a.setSpeed(10000);
-			a.move(100);
-			a.turn(90);
-			a.penDown();
-			for (int i = 1; i <=358 ; i++) {
-				a.turn(1);
-				a.move(1);
-			  }
-			a.penUp();
-			a.move(1000);
-			
-			
-		}
+			String c = JOptionPane.showInputDialog("What color do you want. green, blue, or red");
+			if (c.equals("red")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.move(50);
+				a.setPenColor(255, 0, 0);
+				a.penDown();
+				a.turn(90);
+				a.move(50);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(50);
+				a.penUp();
+				a.turn(-90);
+				a.move(1000);
+			} else if (c.equals("blue")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.move(50);
+				a.setPenColor(0, 0, 255);
+				a.penDown();
+				a.turn(90);
+				a.move(50);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(50);
+				a.penUp();
+				a.turn(-90);
+				a.move(1000);
+			} else if (c.equals("green")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.move(50);
+				a.penDown();
+				a.setPenColor(0, 255, 0);
+				a.turn(90);
+				a.move(50);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(100);
+				a.turn(90);
+				a.move(50);
+				a.penUp();
+				a.turn(-90);
+				a.move(1000);
+			}
+		} else if (s.equals("triangle")) {
+			String c = JOptionPane.showInputDialog("What color do you want. green, blue, or red");
+			if (c.equals("red")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.turn(180);
+				a.move(50);
+				a.setPenColor(255, 0, 0);
+				a.penDown();
+				a.turn(90);
+				a.move(50);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(50);
+				a.penUp();
+				a.turn(90);
+				a.move(1000);
+			} else if (c.equals("blue")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.turn(180);
+				a.move(50);
+				a.setPenColor(0, 0, 255);
+				a.penDown();
+				a.turn(90);
+				a.move(50);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(50);
+				a.penUp();
+				a.turn(90);
+				a.move(1000);
+			} else if (c.equals("green")) {
+				Robot a = new Robot();
+				a.setSpeed(10);
+				a.turn(180);
+				a.move(50);
+				a.setPenColor(0, 255, 0);
+				a.penDown();
+				a.turn(90);
+				a.move(50);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(100);
+				a.turn(120);
+				a.move(50);
+				a.penUp();
+				a.turn(90);
+				a.move(1000);
+			}
+		} else if (s.equals("circle")) {
+			String c = JOptionPane.showInputDialog("What color do you want. green, blue, or red");
+			if (c.equals("red")) {
+				Robot a = new Robot();
+				a.setSpeed(10000);
+				a.move(100);
+				a.turn(90);
+				a.setPenColor(255, 0, 0);
+				a.penDown();
+				for (int i = 1; i <= 358; i++) {
+					a.turn(1);
+					a.move(1);
+				}
+				a.penUp();
+				a.move(1000);
 
+			} else if (c.equals("blue")) {
+				Robot a = new Robot();
+				a.setSpeed(10000);
+				a.move(100);
+				a.turn(90);
+				a.setPenColor(0, 0, 255);
+				a.penDown();
+				for (int i = 1; i <= 358; i++) {
+					a.turn(1);
+					a.move(1);
+				}
+				a.penUp();
+				a.move(1000);
+
+			} else if (c.equals("green")) {
+				Robot a = new Robot();
+				a.setSpeed(10000);
+				a.move(100);
+				a.turn(90);
+				a.setPenColor(0, 255, 0);
+				a.penDown();
+				for (int i = 1; i <= 358; i++) {
+					a.turn(1);
+					a.move(1);
+				}
+				a.penUp();
+				a.move(1000);
+
+			}
+		}
 	}
 }
