d46bea2bfda9e111cd782b6b0d4fa3b05e748871
angelvillarreal1 <league@Mac-mini-3.local>
2018-02-03 11:26:44 -0800

    learned code 16
    fin badger

--- src/Badger.java
+++ src/Badger.java
@@ -1,4 +1,62 @@
 
 public class Badger {
-
+	public static void main(String[] args) {
+		String a = " badger,";
+		String b = " Badger,";
+		String c = " badger";
+		String d = " Mushroom,";
+		String e = " mushroom,";
+		String f = " Mush-";
+		String g = "mushroom, ";
+		String h = " a ";
+		
+		
+		System.out.print(b);
+		for (int i = 1; i <= 8; i++) {
+			System.out.print(a);
+		}
+		System.out.println(c);
+		System.out.print(d);
+		System.out.print(e);
+		System.out.println(h);
+		System.out.print(b);
+		for (int i = 1; i <= 8; i++) {
+			System.out.print(a);
+		}
+		System.out.println(c);
+		System.out.print(d);
+		System.out.print(e);
+		System.out.println(h);
+		System.out.print(b);
+		for (int i = 1; i <= 8; i++) {
+			System.out.print(a);
+		}
+		System.out.println(c);
+		System.out.print(f);
+		System.out.print(g);
+		System.out.println(h);
+		System.out.print(b);
+		for (int i = 1; i <= 8; i++) {
+			System.out.print(a);
+		}
+		System.out.println(c);
+		System.out.println(" African! Snake, a snake\n" + 
+				" Snake! A snake, ooh it's a snake");
+		System.out.print(" It's a");
+		for (int i = 1; i <= 9; i++) {
+			System.out.print(a);
+		}
+		System.out.println(c);
+		System.out.print(d);
+		System.out.print(e);
+		System.out.println(h);
+	}
 }
+// System.out.print();
+// System.out.println();
+
+// String = " ";
+
+// for (int i = 1; i <= 10; i++) {
+// System.out.print();
+// }
